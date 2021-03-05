8b8b6732ddfeb497d16925bb6da771857a060851
Diman142 <Diman142222@gmail.com>
2021-03-05 15:44:11 +0300

    feat: add task desicion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let t = n
+    let a = 1
+    if (n < 0) {
+        a = -1
+        t = a * n
+    }
+
+    return (t.toString().split('').reverse().join(''))
 }
+
